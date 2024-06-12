ca0f6012735788dbd79c7044074254f470d1bcf5
Divyam19 <divyamgupta19@gmail.com>
2024-06-13 01:31:39 +0530

    implemented core Stream Api functionality along with a toaster for updates

--- src/components/MeetingTypeList.tsx
+++ src/components/MeetingTypeList.tsx
@@ -1,18 +1,76 @@
 'use client'
+
 import Image from 'next/image'
 import React, { useState } from 'react'
 import HomeCard from './HomeCard'
 import { useRouter } from 'next/navigation'
 import MeetingModal from './MeetingModal'
-
-const createMeeting=()=>{
-
-}
+import { useUser } from '@clerk/nextjs'
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
+import { Toaster } from "@/components/ui/toaster"
+import { useToast } from './ui/use-toast'
 
 const MeetingTypeList = () => {
     //there is an error in useRouter
     const router=useRouter();
     const [meetingState, setMeetingState] = useState<'isScheduleMeeting'|'isJoiningMeeting'|'isInstantMeeting'|'undefined'>()
+    const {user}=useUser();
+    const client=useStreamVideoClient();
+    const [values,setValues]=useState({
+        dateTime: new Date(),
+        description:"",
+        link:'',
+    });
+
+    const [callDetails, setCallDetails] = useState<Call>()
+    const {toast}=useToast()
+
+    const createMeeting=async ()=>{
+        if(!client || !user) return;
+        
+        try{
+            if(!values.dateTime){
+                toast({
+                    title:'Please select a Date and a time',
+                })
+                return;
+            }
+
+            const id=crypto.randomUUID();
+            const call=client.call('default',id)
+
+            if(!call) throw new Error("Failed to create call");
+
+            const startsAt=values.dateTime.toISOString() || 
+            new Date(Date.now()).toISOString();
+            const description=values.description || 'Instant meeting';
+
+            await call.getOrCreate({
+                data:{
+                    starts_at: startsAt,
+                    custom:{
+                        description
+                    }
+                }
+            })
+
+            setCallDetails(call);
+            if(!values.description){
+                router.push(`/meeting/${call.id}`)
+            }
+
+            toast({
+                title:'Meeting Created',
+            })
+
+        }catch(error){
+            console.log(error);
+            toast({
+                title:'Failed to create meeting',
+            })
+        }
+    }
+
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
         <HomeCard
